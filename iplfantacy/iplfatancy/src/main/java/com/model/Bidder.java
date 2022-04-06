5b1eb38a8c3f8407da5c4b820c1ea7fa0555ea30
Pilla Venkata Pavan <94776226+Longwaytothelife@users.noreply.github.com>
2022-04-06 16:14:55 +0530

    Add files via upload

---
+++ iplfantacy/iplfatancy/src/main/java/com/model/Bidder.java
@@ -0,0 +1,11 @@
+package com.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Bidder {
+	
+
+}
