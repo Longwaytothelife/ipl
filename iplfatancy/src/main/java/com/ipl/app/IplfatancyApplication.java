1e01fff6fda41372626f84e2dec968cae41ff4dc
Pilla Venkata Pavan <94776226+Longwaytothelife@users.noreply.github.com>
2022-04-06 18:06:59 +0530

    Add files via upload

---
+++ iplfatancy/src/main/java/com/ipl/app/IplfatancyApplication.java
@@ -0,0 +1,13 @@
+package com.ipl.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplfatancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplfatancyApplication.class, args);
+	}
+
+}
---
+++ iplfatancy/src/main/java/com/model/Admin.java
@@ -0,0 +1,28 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+	@Id
+	private String userName;
+	private String password;
+	
+	public Admin() {
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
